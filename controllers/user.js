f16de5df4ce26806d509aec44ba99760f35674b3
Jibran Yousuf <jibran.ubit@gmail.com>
2018-08-09 13:29:48 +0500

    Updates

--- controllers/user.js
+++ controllers/user.js
@@ -37,7 +37,7 @@ var UserCtrl = /** @class */ (function (_super) {
                 cnic: req.body.cnic
             }, function (err, user) {
                 if (!user) {
-                    return res.status(404).json({success: false, msg: 'User Not Found'});
+                    return res.json({success: false, msg: 'User Not Found'});
                 }
                 user.comparePassword(req.body.password, function (error, isMatch) {
                     if(err) throw err;
@@ -58,7 +58,7 @@ var UserCtrl = /** @class */ (function (_super) {
                         });
                     }
                     else {
-                        return res.status(401).json({success: false, msg: 'wrong password'});
+                        return res.json({success: false, msg: 'wrong password'});
                     }
                 });
             });
